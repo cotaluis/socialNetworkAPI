8de2fc3692a81b60b28db34fc70a6fe68b209c52
cotaluis <135561226+cotaluis@users.noreply.github.com>
2023-12-13 11:43:40 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParserMiddleware = require('./middleware/bodyParserMiddleware');
+const userRoutes = require('./api/routes/userRoutes');
+const thoughtRoutes = require('./api/routes/thoughtRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect('mongodb://localhost:27017/social_network', { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+app.use(bodyParserMiddleware);
+
+app.use('/api/users', userRoutes);
+app.use('/api/thoughts', thoughtRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
